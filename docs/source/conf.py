72ae55c108709c35887056cb2726d89b7eb12f6f
matampalli satheesh <m.satheesh566@gmail.com>
2020-05-04 11:39:02 +0530

    frst commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,8 @@ author = 'satheesh.m'
 
 # The full version, including alpha/beta/rc tags
 release = '2020'
-
+source_suffix = '.rst'
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
